528bf1539e710157cea19f86f607889645cf649d
Pierre <95082229+system-failed@users.noreply.github.com>
2022-05-03 16:19:44 +0200

    BitbangWith6MPU code
    There is a new code called Bitbang allowing to connect 6MPU to one ESP32. Not giving data to Touch designer yet

--- GyroArduino/TestBitBang/src/main.cpp
+++ GyroArduino/TestBitBang/src/main.cpp
@@ -1,105 +1,116 @@
+//Library to use Arduino cmd
 #include <Arduino.h>
-#include <MPU9250.h>
 #include <SoftWire.h>
 
-MPU9250_<SoftWire> mpu;
-SoftWire sw(27,32);  // change to your own pins
-char swTxBuffer[5000];    // These buffers must be at least as large as
-char swRxBuffer[5000];    // the largest read or write you perform.
+//MPU library
+#include "MPU9250.h"
 
-uint8_t addrs[7] = {0};
-uint8_t device_count = 0;
+//Parameters of the setup
+#define nbrSoftMpu 4
+#define nbrHardMpu 2
 
-template <typename WireType = TwoWire>
-void scan_mpu(WireType& wire = Wire) {
-    Serial.println("Searching for i2c devices...");
-    device_count = 0;
-    for (uint8_t i = 0x68; i < 0x70; ++i) {
-        wire.beginTransmission(i);
-        if (wire.endTransmission() == 0) {
-            addrs[device_count++] = i;
-            delay(10);
-        }
-    }
-    Serial.print("Found ");
-    Serial.print(device_count, DEC);
-    Serial.println(" I2C devices");
+//Addresses and pin of MPU's
+#define MPU_ADDRESS_1 0x68 //Set 0x68 or 0x69
+#define MPU_ADDRESS_2 0x69 //Set 0x68 or 0x69
 
-    Serial.print("I2C addresses are: ");
-    for (uint8_t i = 0; i < device_count; ++i) {
-        Serial.print("0x");
-        Serial.print(addrs[i], HEX);
-        Serial.print(" ");
-    }
-    Serial.println();
-}
+int SDA_PIN[nbrSoftMpu + nbrHardMpu] = {16, 18, 5, 32, 21, 26}; //16 = RX2, 26 and 27 should't be used with Wifi ...
+int SCL_PIN[nbrSoftMpu + nbrHardMpu] = {17, 19, 23, 33, 22, 27}; //17 = TX2
 
-template <typename WireType = TwoWire>
-uint8_t readByte(uint8_t address, uint8_t subAddress, WireType& wire = Wire) {
-    uint8_t data = 0;
-    wire.beginTransmission(address);
-    wire.write(subAddress);
-    // wire.endTransmission(false); // TODO: repeated start fails in SoftWire
-    wire.endTransmission();
-    wire.requestFrom(address, (size_t)1);
-    if (wire.available()) data = wire.read();
-    return data;
-}
+//Software I2c
+MPU9250_<SoftWire> Smpu[nbrSoftMpu]; //Mpu objects for I2c
+SoftWire sw[] = {SoftWire(SDA_PIN[0],SCL_PIN[0]), SoftWire(SDA_PIN[1],SCL_PIN[1]), SoftWire(SDA_PIN[2],SCL_PIN[2]), SoftWire(SDA_PIN[3],SCL_PIN[3])}; //Wire objects for I2C
+
+char swTxBuffer[nbrSoftMpu][64]; //Buffers for softReading, as big as the biggest reading/writing
+char swRxBuffer[nbrSoftMpu][64];
+
+//Hardware I2c
+MPU9250 Hmpu[nbrHardMpu];
 
 void setup() {
-    Serial.begin(115200);
-    Serial.flush();
+  Serial.begin(115200);
+  Serial.flush(); //Clean buffer
 
-    //Seeting up compatibility
-    sw.setTxBuffer(swTxBuffer, sizeof(swTxBuffer));
-    sw.setRxBuffer(swRxBuffer, sizeof(swRxBuffer));
-    sw.setDelay_us(5);
-    sw.begin();
+  //Software I2C
+  for(int i=0; i<nbrSoftMpu; i++) {
+    sw[i].setTxBuffer(swTxBuffer[i], sizeof(swTxBuffer[i]));//Initialisze buffers and connections
+    sw[i].setRxBuffer(swRxBuffer[i], sizeof(swRxBuffer[i]));
+    sw[i].setDelay_us(5);
+    sw[i].begin();
+  }
 
-    //Setting up the gyro
-    MPU9250Setting setting;
-    mpu.setup(0x68, setting, sw);
+  //Hardware I2c
+  Wire.begin(SDA_PIN[4],SCL_PIN[4]);
+  Wire1.begin(SDA_PIN[5], SCL_PIN[5]);
 
-    delay(2000);
 
-    scan_mpu(sw);
+  delay(2000);
 
-    if (device_count == 0) {
-        Serial.println("No device found on I2C bus. Please check your hardware connection");
-        while (1)
-            ;
-    }
+  //MPU parameters (sensitivity, etc)
+  MPU9250Setting setting;
+  setting.accel_fs_sel = ACCEL_FS_SEL::A4G;
+  setting.gyro_fs_sel = GYRO_FS_SEL::G500DPS;
+  setting.mag_output_bits = MAG_OUTPUT_BITS::M16BITS;
+  setting.fifo_sample_rate = FIFO_SAMPLE_RATE::SMPL_200HZ;
+  setting.gyro_fchoice = 0x03;
+  setting.gyro_dlpf_cfg = GYRO_DLPF_CFG::DLPF_41HZ;
+  setting.accel_fchoice = 0x01;
+  setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
 
-    // check WHO_AM_I address of MPU
-    for (uint8_t i = 0; i < device_count; ++i) {
-        Serial.print("I2C address 0x");
-        Serial.print(addrs[i], HEX);
-        byte ca = readByte(addrs[i], WHO_AM_I_MPU9250, sw);
-        if (ca == MPU9250_WHOAMI_DEFAULT_VALUE) {
-            Serial.println(" is MPU9250 and ready to use");
-        } else if (ca == MPU9255_WHOAMI_DEFAULT_VALUE) {
-            Serial.println(" is MPU9255 and ready to use");
-        } else if (ca == MPU6500_WHOAMI_DEFAULT_VALUE) {
-            Serial.println(" is MPU6500 and ready to use");
-        } else {
-            Serial.println(" is not MPU series");
-            Serial.print("WHO_AM_I is ");
-            Serial.println(ca, HEX);
-            Serial.println("Please use correct device");
-        }
-        static constexpr uint8_t AK8963_ADDRESS {0x0C};  //  Address of magnetometer
-        static constexpr uint8_t AK8963_WHOAMI_DEFAULT_VALUE {0x48};
-        byte cb = readByte(AK8963_ADDRESS, AK8963_WHO_AM_I);
-        if (cb == AK8963_WHOAMI_DEFAULT_VALUE) {
-            Serial.print("AK8963 (Magnetometer) is ready to use");
-        } else {
-            Serial.print("AK8963 (Magnetometer) was not found");
-        }
-    }
+  //Launch mpu setup
+  for(int i=0; i<nbrSoftMpu; i++) {
+    Smpu[i].setup(MPU_ADDRESS_1, setting, sw[i]);
+  }
+  Hmpu[0].setup(MPU_ADDRESS_1, setting, Wire);
+  Hmpu[1].setup(MPU_ADDRESS_1, setting, Wire1);
+
+  //Selection of filters
+  /*QuatFilterSel sel{QuatFilterSel::MADGWICK};
+  mpu1.selectFilter(sel);
+  mpu1.setFilterIterations(10);*/
+  
+
+
+  /*Serial.println("Calibration of acceleration : don't move devices");
+  //mpu1.calibrateAccelGyro();
+  Serial.println("Acceleration calibration done.");
+
+  Serial.println("Calibration of mag 1");
+  mpu1.setMagneticDeclination(2.53);
+  //mpu1.calibrateMag();
+  Serial.println("Mag calibration done.");
+
+  mpu1.setFilterIterations(10);
+  QuatFilterSel sel{QuatFilterSel::MADGWICK};
+  mpu1.selectFilter(sel);*/
+
+
 }
 
 void loop() {
-  /*Serial.print(mpu.getEulerX());
-  Serial.print(mpu.getEulerY());
-  Serial.println(mpu.getEulerZ());*/
+  //--------MPU recording--------
+  
+  //Read mpu data
+  Smpu[0].update();
+  Smpu[1].update();
+  Smpu[2].update();
+  Smpu[3].update();
+  Hmpu[0].update();
+  Hmpu[1].update();
+
+  static unsigned long last_print=0;
+  
+  
+  if (millis()-last_print > 100) {
+
+        Serial.print(Smpu[0].getEulerX()); Serial.print("// ");
+        Serial.print(Smpu[1].getEulerX()); Serial.print("// ");
+        Serial.print(Smpu[2].getEulerX()); Serial.print("// ");
+        Serial.print(Smpu[3].getEulerX()); Serial.print("// ");
+        Serial.print(Hmpu[0].getEulerX()); Serial.print("// ");
+        Serial.print(Hmpu[1].getEulerX()); Serial.print("// ");
+
+        Serial.println();
+
+        last_print=millis();
+  }
 }
