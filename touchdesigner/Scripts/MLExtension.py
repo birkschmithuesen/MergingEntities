250921e310306907e1e36e2191d8a113112fd503
Birk Schmithuesen | DesktopTD <contact@birkschmithuesen.com>
2022-05-24 18:18:30 +0200

    Update touchdesigner folder from birk-dev

--- touchdesigner/Scripts/MLExtension.py
+++ touchdesigner/Scripts/MLExtension.py
@@ -12,6 +12,8 @@ import sys
 import os
 import platform
 
+from io import StringIO
+
 import tensorflow as tf
 import keras
 from tensorflow.keras.models import Sequential
@@ -20,6 +22,8 @@ from tensorflow.keras.optimizers import SGD
 from tensorflow.python.client import device_lib
 
 import numpy as np
+
+import json
 
 from TDStoreTools import StorageManager
 import TDFunctions as TDF
@@ -31,133 +35,187 @@ class MLExtension:
 	def __init__(self, ownerComp):
 		# The component to which this extension is attached
 		self.ownerComp = ownerComp
-
-		# Environment Settings
-		TDF.createProperty(self, 'Username', value='', dependable=True,
-						   readOnly=False)
 		
-		TDF.createProperty(self, 'EnvName', value='', dependable=True,
-						   readOnly=False)
-
-		TDF.createProperty(self, 'EnvLocation', value='', dependable=True,
-						   readOnly=False)
-
-		TDF.createProperty(self, 'Model', value='', dependable=True,
-						   readOnly=False)
+		# Model
+		self.Model = ''
 
 		# Model Settings
-		TDF.createProperty(self, 'INPUT_DIM', value='', dependable=True,
-						   readOnly=False)
-		
-		TDF.createProperty(self, 'OUTPUT_DIM', value='', dependable=True,
-						   readOnly=False)
+		self.Modeltype = tdu.Dependency(0)
+		self.Modeltypes = ['linear_regression','lstm']
+		self.Modelname = self.Modeltypes[self.Modeltype]
 
-		TDF.createProperty(self, 'BATCH_SIZE', value='', dependable=True,
-						   readOnly=False)
+		TDF.createProperty
 
-		TDF.createProperty(self, 'EPOCHS', value='', dependable=True,
-						   readOnly=False)
+		self.Inputdim = tdu.Dependency(67)
+		self.Outputdim = tdu.Dependency(8)
+		self.Batchsize= tdu.Dependency(1)
+		self.Epochs = tdu.Dependency(1)
+		self.Initialepochs = tdu.Dependency(1)
+		self.Hiddendim = tdu.Dependency(10)
+		self.Learningrate = tdu.Dependency(1)
+		self.Timesteps = tdu.Dependency(8)
 
-		TDF.createProperty(self, 'INITIAL_EPOCHS', value='', dependable=True,
-						   readOnly=False)
-		
-		TDF.createProperty(self, 'HIDDEN_DIM', value='', dependable=True,
-						   readOnly=False)
-		
-		TDF.createProperty(self, 'LEARNING_RATE', value='', dependable=True,
-						   readOnly=False)
+		# Selected Data Points
+		self.Selectedfeatures = tdu.Dependency('^sound_*')
+		self.Selectedtargets = tdu.Dependency('sound_*')
+		self.DatapointsList = ''		   
 
 		# Training Settings
-		TDF.createProperty(self, 'TrainingFileLocation', value='', dependable=True,
-						   readOnly=False)
-		TDF.createProperty(self, 'Features', value='', dependable=True,
-						   readOnly=False)
-		TDF.createProperty(self, 'Targets', value='', dependable=True,
-						   readOnly=False)
-		
-		self.GetEnvSettings()
+		self.Trainingdata = tdu.Dependency('Entity-Recordings/default.txt')
+		self.Features = ''
+		self.Targets = ''
 
+		# Config Attributes
+		config_attributes = [ 
+			
+			"Modeltype",
+			"Modelname",
+			"Inputdim",
+			"Outputdim",
+			"Batchsize",
+			"Epochs",
+			"Initialepochs",
+			"Hiddemdim",
+			"Learningrate",
+			"Timesteps"
+
+		]
+
+		for val  in config_attributes:
+			debug(val)
+		
+		#Check if CUDA and GPU is enabled
 		debug('Is TensorFlow built with CUDA: ', tf.test.is_built_with_cuda())
 		debug('Is GPU available: ', tf.test.is_gpu_available(cuda_only=False, min_cuda_compute_capability=None))
 		debug('Num GPUs Available: ', len(tf.config.experimental.list_physical_devices('GPU')))
 		debug('List of local devices: ', device_lib.list_local_devices())
-		
-
-	def GetEnvSettings(self):
-		self.Username = parent.Ml.par.Windowsusername
-		self.EnvName = parent.Ml.par.Condaenvironmentname
-		self.EnvLocation = parent.Ml.par.Environmentfolder
-		debug(self.Username, self.EnvName, self.EnvLocation)
-
-	def InitiateCondaEnvironment(self):
-		if platform.system() == 'Windows':
-			if sys.version_info.major >= 3 and sys.version_info.minor >= 8:
-				"""
-				Double check all the following paths, it could be that your anaconda 'envs' folder is not in your User folder depending on your conda install settings and conda version.
-				"""
-				os.add_dll_directory('C:/Users/'+self.Username+'/anaconda3/envs/'+self.EnvName+'/DLLs')
-				os.add_dll_directory('C:/Users/'+self.Username+'/anaconda3/envs/'+self.EnvName+'/Library/bin')
-			else:
-				"""
-				Double check all the following paths, it could be that your anaconda 'envs' folder is not in your User folder depending on your conda install settings and conda version.
-				"""
-				# Not the most elegant solution, but we need to control load order
-				os.environ['PATH'] = 'C:/Users/'+self.Username+'/anaconda3/envs/'+self.EnvName+'/DLLs' + os.pathsep + os.environ['PATH']
-				os.environ['PATH'] = 'C:/Users/'+self.Username+'/anaconda3/envs/'+self.EnvName+'/Library/bin' + os.pathsep + os.environ['PATH']
-
-		else:
-			"""
-			MacOS users should include path to .dlybs / MacOS binaries
-			"""
-			debug("No MAC os")
-
-		sys.path = ['C:/Users/'+self.Username+'/anaconda3/envs/'+self.EnvName+'/Lib/site-packages'] + sys.path
-		debug("Finished Initiation")
 	
 	def InitiateModel(self):
 		self.Model = Sequential()
 		debug("Initiated Model")
+
+	def ModelName(self):
+		self.Modelname = self.Modeltypes[self.Modeltype]
+		return self.Modelname
 	
 	def GetModelSettings(self):
-		self.INPUT_DIM = int(parent.Ml.par.Inputdim)
-		self.OUTPUT_DIM = int(parent.Ml.par.Outputdim)
-		self.BATCH_SIZE = int(parent.Ml.par.Batchsize)
-		self.EPOCHS = int(parent.Ml.par.Epochs)
-		self.INITIAL_EPOCHS = int(parent.Ml.par.Initialepochs)
-		self.HIDDEN_DIM = int(parent.Ml.par.Hiddendim)
-		self.LEARNING_RATE = float(parent.Ml.par.Learningrate)
+		# Get Settings from Data Selection
+		self.DatapointsList = op('selected_data_points').row(0)
+		# Lookup Model Name from List
+		self.ModelName()
+		debug("Get Model Settings")
 	
 	def BuildModel(self):
-		my_init=keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None)
-		self.Model.add(Dense(self.HIDDEN_DIM, activation='sigmoid', input_dim=self.INPUT_DIM, kernel_initializer=my_init, bias_initializer=my_init))
-		self.Model.add(Dense(self.OUTPUT_DIM, activation='sigmoid',kernel_initializer=my_init, bias_initializer=my_init))
-		sgd = SGD(learning_rate=self.LEARNING_RATE, decay=1e-6, momentum=0.9, nesterov=True)
-		self.Model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
-		debug("Built Model")
-
-	def GetTrainingFileLocation(self):
-		self.TrainingFileLocation = parent.Ml.par.Trainingdata
+		if self.Modelname == 'linear_regression':
+			my_init=keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None)
+			self.Model.add(Dense(self.Hiddendim.val, activation='sigmoid', input_dim=self.Inputdim.val, kernel_initializer=my_init, bias_initializer=my_init))
+			self.Model.add(Dense(self.Outputdim.val, activation='sigmoid',kernel_initializer=my_init, bias_initializer=my_init))
+			sgd = SGD(learning_rate=self.Learningrate.val, decay=1e-6, momentum=0.9, nesterov=True)
+			self.Model.compile(loss='binary_crossentropy', optimizer=sgd, metrics=['accuracy'])
+		elif self.Modelname == 'lstm':
+			self.Model.add(LSTM(units=128, batch_input_shape=(self.Batchsize.val, self.Timesteps.val, self.Inputdim.val), stateful=True, return_sequences=True))
+			self.Model.add(LSTM(units=128, batch_input_shape=(self.Batchsize.val, self.Timesteps.val, self.Inputdim.val), stateful=True, return_sequences=False))
+			self.Model.add(Dense(units=self.Outputdim.val, activation='sigmoid'))
+			self.Model.compile(optimizer='rmsprop',loss='mse')
+		debug("Built ", self.ModelName(), " Model")
 
 	def LoadTrainingData(self):
-		file_loc = str(self.TrainingFileLocation)
-		file = open(file_loc)
-		values = np.loadtxt(file_loc, skiprows=1, dtype='float32')
+		#file_loc = str(self.TrainingFileLocation)
+		#file = open(file_loc)
+		#values = np.loadtxt(file_loc, skiprows=1, dtype='float32')
+		string_values = StringIO(op('selected_data').text)
+		values = np.loadtxt(string_values,skiprows=1,dtype='float32')
 		debug('Training Data Points: ', values.shape[0])
-		self.Features, self.Targets = values[:,:-self.OUTPUT_DIM], values[:,self.INPUT_DIM:]
+		self.Features, self.Targets = values[:,:-self.Outputdim.val], values[:,self.Inputdim.val:]
+		if self.Modelname == 'lstm':
+			self.Features = self.rolling_window2D(self.Features,self.Timesteps.val)
+			#self.Targets = self.Targets[self.Timesteps.val-1:]
+			#adjusting to batch_size
+			length, y, z = self.Features.shape
+			offset = length % self.Batchsize
+			feature_length = length - offset - self.Batchsize
+			self.Features = self.Features[0:feature_length, ]
+			self.Targets = self.Targets[self.Timesteps-1:feature_length+self.Timesteps-1, ]
 		debug('Training Features Shape: ', self.Features.shape, 'Training Targets Shape: ', self.Targets.shape)
 
+	def rolling_window2D(self,a,n):
+		# a: 2D Input array 
+		# n: Group/sliding window length
+		return a[np.arange(a.shape[0]-n+1)[:,None] + np.arange(n)]
+
 	def FitModel(self):
-		self.Model.fit(self.Features,self.Targets,epochs=self.INITIAL_EPOCHS,batch_size=self.BATCH_SIZE,shuffle=True)
-		self.Model.summary()
+		debug("Fitting Model")
+		if self.Modelname == 'linear_regression':
+			debug("Starting Linear Regression Fit")
+			self.Model.fit(self.Features,self.Targets,epochs=self.Initialepochs.val,batch_size=self.Batchsize.val,shuffle=True)
+		elif self.Modelname == 'lstm':
+			debug("Starting LSTM Fit")
+			try:
+				self.Model.fit(x=self.Features,y=self.Targets,batch_size=self.Batchsize.val,epochs=self.Initialepochs.val,use_multiprocessing=False)
+				# save tmp weights to later set into new model with batchsize 1
+				tmp_model_weights = self.Model.get_weights()
+				# set new batchsize
+				self.Batchsize.val = 1
+				# make new model
+				self.Model = Sequential()
+				self.Model.add(LSTM(units=128, batch_input_shape=(self.Batchsize.val, self.Timesteps.val, self.Inputdim.val), stateful=True, return_sequences=True))
+				self.Model.add(LSTM(units=128, batch_input_shape=(self.Batchsize.val, self.Timesteps.val, self.Inputdim.val), stateful=True, return_sequences=False))
+				self.Model.add(Dense(units=self.Outputdim.val, activation='sigmoid'))
+				# set weights from tmp model
+				self.Model.set_weights(tmp_model_weights)
+				self.Model.compile(optimizer='rmsprop',loss='mse')
+			except ValueError as e:
+				debug("Couldn't Fit Model", e)
+		#self.Model.summary()
 		debug("Initial Training finished... ")
-	
+
 	def SaveModel(self):
 		location = parent.Ml.par.Modelname
 		self.Model.save('Models/' + location) #saving as SavedModel format
+		self.CreateModelConfigFile()
+		debug("Saved Model")
+
+	def CreateModelConfigFile(self):
+		location = parent.Ml.par.Modelname
+		json_config = {}
+		json_config['Model_Type'] = self.Modeltype.val
+		json_config['Training_Data'] = self.Trainingdata.val
+		json_config['Selected_Features'] = self.Selectedfeatures.val
+		json_config['Selected_Targets'] = self.Selectedtargets.val
+		json_config['INPUT_DIM'] = self.Inputdim.val
+		json_config['OUTPUT_DIM'] = self.Outputdim.val
+		json_config['BATCH_SIZE'] = self.Batchsize.val
+		json_config['EPOCHS'] = self.Epochs.val
+		json_config['INITIAL_EPOCHS'] = self.Initialepochs.val
+		json_config['HIDDEN_DIM'] = self.Hiddendim.val
+		json_config['LEARNING_RATE'] = self.Learningrate.val
+		json_config['TIME_STEPS'] = self.Timesteps.val
+		with open('Models/' + location + '/model_config.json', 'w') as jsonFile:
+			json.dump(json_config,jsonFile)
 
 	def LoadModel(self):
 		model_folder = str(parent.Ml.par.Selectmodel)
 		self.Model = keras.models.load_model(model_folder)
+		self.LoadSettingsFromConfigFile()
+		debug("Loaded Model")
+
+	def LoadSettingsFromConfigFile(self):
+		op('load_model_config').par.refreshpulse.pulse()
+		model_config = op('model_config')
+		self.Modeltype.val = model_config.result['Model_Type']
+		self.Modeltype.modified()
+		self.ModelName()
+		self.Trainingdata.val = model_config.result['Training_Data']
+		self.Selectedfeatures.val = model_config.result['Selected_Features']
+		self.Selectedtargets.val = model_config.result['Selected_Targets']
+		self.Inputdim.val = model_config.result['INPUT_DIM']
+		self.Outputdim.val = model_config.result['OUTPUT_DIM']
+		self.Batchsize.val = model_config.result['BATCH_SIZE']
+		self.Epochs.val = model_config.result['EPOCHS']
+		self.Initialepochs.val = model_config.result['INITIAL_EPOCHS']
+		self.Hiddendim.val = model_config.result['HIDDEN_DIM']
+		self.Learningrate.val = model_config.result['LEARNING_RATE']
+		self.Timesteps.val = model_config.result['TIME_STEPS']
 
 	def PredictTargets(self,features):
 		return self.Model.predict(np.array([features]))
