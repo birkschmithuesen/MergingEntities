ae2ad0c25701f51f67b44ccc165dbfc99b93c749
tpltnt <1172976+tpltnt@users.noreply.github.com>
2023-03-30 22:24:58 +0200

    reimplemented countMultiplexer()

--- GyroArduino/firmwares/OneESP_SixMPU9250/src/main.cpp
+++ GyroArduino/firmwares/OneESP_SixMPU9250/src/main.cpp
@@ -494,16 +494,66 @@ void automaticMagnetometerCalibration() {
  * @see countI2cDevices()
  * @see selectI2cMultiplexerChannel(uint8_t address, uint8_t channel)
  */
-uint8_t countMultiplexer(){
-  uint8_t count = 0;
-  if (selectI2cMultiplexerChannel(TCA_ADDRESS_RIGHT_SIDE, 1)) {
-    count = 1;
+uint8_t countMultiplexer() {
+  uint8_t deviceCount = 0;
+  uint8_t result = 0;
+
+#ifdef DEBUG
+  Serial.println();
+  Serial.print("* I2C timeout is ");
+  Serial.print(Wire.getTimeOut());
+  Serial.println("ms");
+  Serial.print("* address 0x");
+  Serial.println(TCA_ADDRESS_LEFT_SIDE, HEX);
+#endif
+  // we try to talk to expected multiplexers
+  Wire.beginTransmission(TCA_ADDRESS_LEFT_SIDE);
+  result = Wire.endTransmission(true);
+  switch(result) {
+  case 0:
+    // talking was successful, we found a device at the current address
+    deviceCount += 1;
+#ifdef DEBUG
+    Serial.println("* found a multiplexer");
+    break;
+  case 5:
+    Serial.println("* I2C bus timeout");
+#endif
+    break;
+  default:
+    break;
   }
-  if (selectI2cMultiplexerChannel(TCA_ADDRESS_LEFT_SIDE, 1)) {
-    count = 2;
+
+  // doing it again
+#ifdef DEBUG
+  Serial.print("* I2C timeout is ");
+  Serial.print(Wire.getTimeOut());
+  Serial.println("ms");
+  Serial.print("* address 0x");
+  Serial.println(TCA_ADDRESS_RIGHT_SIDE, HEX);
+#endif
+  // doing it again
+  Wire.beginTransmission(TCA_ADDRESS_RIGHT_SIDE);
+  result = Wire.endTransmission(true);
+  switch(result) {
+  case 0:
+    // talking was successful, we found a device at the current address
+    deviceCount += 1;
+#ifdef DEBUG
+    Serial.println("* found a multiplexer");
+    break;
+  case 5:
+    Serial.println("* I2C bus timeout");
+#endif
+    break;
+  default:
+    break;
   }
-  return count;
+
+  //delay(5000); // wait 5 seconds for next scan - TODO: remove?
+  return deviceCount;
 }
+
 
 /**
  * Check which MPU9250 sensor board is there and do the initial
